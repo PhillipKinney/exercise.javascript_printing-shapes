cfe25893192ef33332777824a8f1eb8d84d5d719
PhillipKinney <phillipjkinney@gmail.com>
2020-06-29 16:55:20 -0400

    still on 2nd shape. Coming along slowly....

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -1,11 +1,38 @@
 function getLine(length) {
-    // TODO - write method definition here
+    TODO - write method definition here
+    length = 0;
+    
+let output ="";
+for (let i=0; i<length; i++){
+output +="*"
+}
+return output;
+
 }
 
 
 
 function getBox(width, height) {
     // TODO - write method definition here
+
+    
+
+    let wOutput="";
+    let hOutput="";
+    output= hOutput + wOutput
+
+    for (let i=0; i<width; i++){
+        wOutput+= "*"
+       }
+
+
+    for (let k=0; k<height; k++){
+        hOutput+="*\n"
+    }
+
+    
+    document.write(hOutput, wOutput);
+
 }
 
 
